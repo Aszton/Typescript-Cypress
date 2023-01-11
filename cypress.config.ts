bfa85876cdb62798ac4c0f7afc8c3f5b72996674
Aszton <bartoszkuczera88@gmail.com>
2023-01-11 21:19:50 +0100

    changed cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,8 +4,8 @@ export default defineConfig({
   e2e: {
     viewportWidth: 1280,
     viewportHeight: 720,
-    defaultCommandTimeout: 5000,
-    requestTimeout: 5000,
+    defaultCommandTimeout: 7000,
+    requestTimeout: 7000,
     projectId: "t7mkm6",
     setupNodeEvents(on, config) {
       require("cypress-mochawesome-reporter/plugin")(on);
